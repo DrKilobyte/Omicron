c8a62e376dc71bd570e7b27a27071fe9c8873e25
Dr. Kilobyte <drkilobyte@drkilobyte.com>
2023-06-01 20:12:11 -0500

    Added a slew of new instructions

--- omicron.py
+++ omicron.py
@@ -1,4 +1,4 @@
-import sys
+import sys, math
 if len(sys.argv) > 1:
     program = [c.strip() for c in open(sys.argv[1], 'r').read().split()]
     memory = {}
@@ -65,6 +65,42 @@ if len(sys.argv) > 1:
             elif instruction == '\\':
                 memory[pointer] = memory[pointer]**(1/mem(program[i+1]))
                 i += 1
+            elif instruction == 'round':
+                memory[pointer] = round(memory[pointer])
+            elif instruction == 'ceil':
+                memory[pointer] = math.ceil(memory[pointer])
+            elif instruction == 'floor':
+                memory[pointer] = math.floor(memory[pointer])
+            elif instruction == 'ceil':
+                memory[pointer] = math.ceil(memory[pointer])
+            elif instruction == 'sin':
+                memory[pointer] = math.sin(memory[pointer])
+            elif instruction == 'cos':
+                memory[pointer] = math.cos(memory[pointer])
+            elif instruction == 'tan':
+                memory[pointer] = math.tan(memory[pointer])
+            elif instruction == 'log':
+                memory[pointer] = math.log(memory[pointer], mem(program[i+1]))
+                i += 1
+            elif instruction == 'abs': memory[pointer] = abs(memory[pointer])
+            elif instruction == 'not': memory[pointer] = int(not memory[pointer])
+            elif instruction == 'pi': memory[pointer] = math.pi
+            # Comparison
+            elif instruction == 'eq':
+                memory[pointer] = int(memory[pointer]==mem(program[i+1]))
+                i += 1
+            elif instruction == 'gt':
+                memory[pointer] = int(memory[pointer]>mem(program[i+1]))
+                i += 1
+            elif instruction == 'gte':
+                memory[pointer] = int(memory[pointer]>=mem(program[i+1]))
+                i += 1
+            elif instruction == 'lt':
+                memory[pointer] = int(memory[pointer]<mem(program[i+1]))
+                i += 1
+            elif instruction == 'lte':
+                memory[pointer] = int(memory[pointer]<=mem(program[i+1]))
+                i += 1
             # Flow
             elif instruction == 'goto':
                 try: i = markers[mem(program[i+1])]-1
@@ -81,8 +117,27 @@ if len(sys.argv) > 1:
             elif instruction == 'inputc':
                 stdin = input()
                 memory[pointer] = ord(stdin[0]) if len(stdin) > 0 else 0
+            elif instruction == 'read':
+                with open(program[i+1], 'rb') as f:
+                    memory[pointer] = f.read()[mem(program[i+2])]
+                i += 2
+            elif instruction == 'size':
+                with open(program[i+1], 'rb') as f: memory[pointer] = len(f.read())
+                i += 1
             elif instruction == 'print': print(memory[pointer])
             elif instruction == 'printc': print(chr(memory[pointer]), end="", flush=True)
+            elif instruction == 'write':
+                with open(program[i+1], 'w') as f: f.write(chr(memory[pointer]))
+                i += 1
+            elif instruction == 'awrite':
+                with open(program[i+1], 'a') as f: f.write(chr(memory[pointer]))
+                i += 1
+            elif instruction == 'writeb':
+                with open(program[i+1], 'wb') as f: f.write(bytearray([memory[pointer]]))
+                i += 1
+            elif instruction == 'awriteb':
+                with open(program[i+1], 'ab') as f: f.write(bytearray([memory[pointer]]))
+                i += 1
 
             elif instruction == 'stop': break
             elif instruction.startswith(":"): pass
